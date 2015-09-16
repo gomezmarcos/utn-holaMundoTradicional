6b8a9d5e58a75778c84db08f765291ce0a585637
marcos <margomez@agea.com.ar>
2015-09-16 12:06:58 -0300

    proyecto tradicional para comparar con MVC

---
+++ app/src/main/java/com/example/marcos/holamundotradicional/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.marcos.holamundotradicional;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText input = (EditText) findViewById(R.id.input);
+        final TextView output = (TextView) findViewById(R.id.output);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                output.setText(input.getText().toString());
+            }
+        });
+    }
+
+}
